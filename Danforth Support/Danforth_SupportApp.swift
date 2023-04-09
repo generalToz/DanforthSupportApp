d7bd3f2854d5f2cfac0238b927e4b6008019bdb7
Alex Tosspon <tossyourwords@me.com>
2023-04-08 20:41:41 -0500

    Initial Commit

---
+++ Danforth Support/Danforth_SupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Danforth_SupportApp.swift
+//  Danforth Support
+//
+//  Created by Alex Tosspon on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Danforth_SupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
